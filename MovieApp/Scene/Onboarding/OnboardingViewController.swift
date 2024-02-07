fa0df5ef5c94ce00ca68a72a34b4bacc773dd83d
Mert Polat <mert.dev@icloud.com>
2024-02-07 23:16:59 +0300

    icon images changed from png to pdf

---
+++ MovieApp/Scene/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingViewController.swift
+//  MovieApp
+//
+//  Created by mert polat on 7.02.2024.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
