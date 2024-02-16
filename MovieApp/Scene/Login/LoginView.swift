4aae3ab8341d136dd5733376f355221649608685
Mert Polat <mert.dev@icloud.com>
2024-02-16 16:35:04 +0300

    Created part of the login screen

--- MovieApp/Scene/Login/LoginView.swift
+++ MovieApp/Scene/Login/LoginView.swift
@@ -6,24 +6,107 @@
 //
 
 import UIKit
+import TinyConstraints
 
-class LoginView: UIViewController {
+final class LoginView: UIViewController {
+    
+    let backgroundImageView : UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = .loginBackground
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let titleLabel : UILabel = {
+       let label = UILabel()
+        label.font = FontManager.headline1()
+        label.text = "Login"
+        label.textColor = ColorManager.surfaceLight
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let emailTextField : MaTextField = {
+      let textField = MaTextField()
+        textField.style = .email
+        return textField
+    }()
+    
+    let emailLineView : UIView = {
+       let view = UIView()
+        view.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.12)
+
+        view.height(1.5)
+        view.alpha = 0.12
+        view.width(335)
+        return view
+    }()
+    
+    let passwordTextField : MaTextField = {
+      let textField = MaTextField()
+        textField.style = .password
+        return textField
+    }()
+    
+    let passwordLineView : UIView = {
+       let view = UIView()
+        view.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.12)
+
+        view.height(1.5)
+        view.alpha = 0.12
+        view.width(335)
+        return view
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func configureUI(){
+        view.backgroundColor = ColorManager.surfaceDark
+        view.addSubview(backgroundImageView)
+        view.addSubview(titleLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(emailLineView)
+        view.addSubview(passwordTextField)
+        view.addSubview(passwordLineView)
+        
+        backgroundImageView.topToSuperview(offset: 0)
+        backgroundImageView.leadingToSuperview(offset: 0)
+        backgroundImageView.trailingToSuperview(offset: 0)
+        
+        titleLabel.leadingToSuperview(offset: 20)
+        titleLabel.topToBottom(of: backgroundImageView,offset: 33)
+        
+        emailTextField.leadingToSuperview(offset: 20)
+        emailTextField.topToBottom(of: titleLabel,offset: 25)
+        
+        emailLineView.topToBottom(of: emailTextField,offset: 0)
+        emailLineView.leading(to: emailTextField)
+        emailLineView.trailing(to: emailTextField)
+        
+        passwordTextField.topToBottom(of: emailLineView,offset: 15)
+        passwordTextField.leading(to: emailTextField)
+        passwordTextField.trailing(to: emailTextField)
+        
+        passwordLineView.topToBottom(of: passwordTextField,offset: 0)
+        passwordLineView.leading(to: passwordTextField)
+        passwordLineView.trailing(to: passwordTextField)
+        
+        
+        
     }
-    */
 
+    
+}
+
+
+
+#Preview {
+    LoginView()
 }
