7ec4cf1fbfe4830e3b6d628f8ccf736b01ab597c
Mert Polat <mert.dev@icloud.com>
2024-03-13 15:57:35 +0300

    router added for Register screen

--- MovieApp/Scene/Register/RegisterRoute.swift
+++ MovieApp/Scene/Register/RegisterRoute.swift
@@ -5,4 +5,22 @@
 //  Created by mert polat on 6.03.2024.
 //
 
-import Foundation
+import UIKit
+
+protocol RegisterRoute {
+    func placeOnRegisterViewController()
+}
+
+extension RegisterRoute where Self: RouterProtocol {
+    
+    func placeOnRegisterViewController() {
+        let router = RegisterRouter()
+        let registerViewController = RegisterView(router: router)
+        let navigationController = UINavigationController(rootViewController: registerViewController)
+        let transition = PlaceOnWindowTransition()
+        
+        router.presentingViewController = registerViewController
+        
+        open(navigationController, transition: transition)
+    }
+}
--- MovieApp/Scene/Register/RegisterRouter.swift
+++ MovieApp/Scene/Register/RegisterRouter.swift
@@ -5,4 +5,8 @@
 //  Created by mert polat on 6.03.2024.
 //
 
-import Foundation
+import UIKit
+
+final class RegisterRouter: Router, RegisterRouter.Routes {
+    typealias Routes = LoginRoute & ForgotPasswordRoute & HomeRoute
+}
