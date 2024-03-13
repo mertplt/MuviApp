e3b69fdb4855676765864851ef2f29ec681e9646
Mert Polat <mert.dev@icloud.com>
2024-03-13 15:57:35 +0300

    router added for Register screen

--- MovieApp/Scene/Register/RegisterView.swift
+++ MovieApp/Scene/Register/RegisterView.swift
@@ -8,22 +8,23 @@
 import UIKit
 
 class RegisterView: UIViewController {
-
+    var registerViewModel: RegisterViewModel!
+    var router: RegisterRouter
+    
+    init(router: RegisterRouter) {
+        self.router = router
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     override func viewDidLoad() {
+        view.backgroundColor = .red
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- MovieApp/Scene/Register/RegisterViewModel.swift
+++ MovieApp/Scene/Register/RegisterViewModel.swift
@@ -5,4 +5,32 @@
 //  Created by mert polat on 2.03.2024.
 //
 
-import Foundation
+import UIKit
+
+class RegisterViewModel {
+//    var loginStatus: Observable<String> = Observable("")
+    
+    
+    func register(loginModel: LoginModel) {
+        // Giriş verilerini doğrulama ve giriş işlemini gerçekleştirme kodları
+        if loginModel.email.isEmpty || loginModel.password.isEmpty {
+//            loginStatus.value = "Username or password cannot be empty"
+            return
+        }
+        
+//        loginStatus.value = "Logging in..."
+        
+        // Sunucu ile giriş işlemini gerçekleştirme kodları
+        // Bu örnekte basitçe başarılı olduğunu varsayalım
+        let success = true
+        
+        if success {
+//            loginStatus.value = "Login successful"
+//            // Router ile ana sayfaya yönlendirme kodları
+//            LoginRouter.navigateToRegister()
+        } else {
+//            loginStatus.value = "Login failed"
+        }
+    }
+}
+
