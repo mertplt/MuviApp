3da9ec2e72abf009cc2fbc3365bfcf8e470d5f54
mertpolat <mert.dev@icloud.com>
2024-08-08 10:31:25 +0300

    Added IQKeyboardManager

--- MovieApp/App/AppDelegate.swift
+++ MovieApp/App/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = ColorManager.dark
-
+        
+        IQKeyboardManager.shared.enable = true
         return true
     }
     
